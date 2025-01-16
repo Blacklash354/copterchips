d0a25958985aee6385a9efc52c2654140b2ce46b
Blacklash354 <68793668+Blacklash354@users.noreply.github.com>
2025-01-17 01:42:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const obstacleGap = 150;
 const obstacleSpeed = 3;
 
 const backgroundImage = new Image();
-backgroundImage.src = 'background.png';
+backgroundImage.src = 'assets/images/background.png';
 
 function createObstacle() {
     const height = Math.random() * (canvas.height - obstacleGap);
